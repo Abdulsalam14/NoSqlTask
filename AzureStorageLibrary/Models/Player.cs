34a8d4e93334f3f6409cfb732f1f39095a513d4c
Abdulsalam14 <abdullayevabdulsalam@gmail.com>
2024-03-27 08:07:54 +0400

    Add project files.

---
+++ AzureStorageLibrary/Models/Player.cs
@@ -0,0 +1,24 @@
+﻿
+using Azure;
+using Azure.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorageLibrary.Models
+{
+    public class Player:ITableEntity
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? BirthDate { get; set; }
+        public double Salary { get; set; }
+        public double Score { get; set; }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; } = DateTimeOffset.MinValue;
+        public ETag ETag { get; set; } = ETag.All;
+    }
+}
---
+++ MVCWebApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using AzureStorageLibrary.Models;
+using AzureStorageLibrary.Services;
+using Microsoft.AspNetCore.Mvc;
+using MVCWebApp.Models;
+using System.Diagnostics;
+
+namespace MVCWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly INoSqlStorage<Player> _noSqlStorage;
+
+        public HomeController(INoSqlStorage<Player> noSqlStorage)
+        {
+            _noSqlStorage = noSqlStorage;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var player = new Player
+            {
+                RowKey = Guid.NewGuid().ToString(),
+                PartitionKey = "Players",
+                Name="Player1",
+                Surname="Player1 Surname",
+                BirthDate=new DateOnly(1994,04,04).ToString(),
+                Score=11.4,
+                Salary=4000
+            };
+            await _noSqlStorage.Add(player);
+            ViewBag.players = (await _noSqlStorage.All()).ToList();
+            return View();
+        }
+    }
+}
